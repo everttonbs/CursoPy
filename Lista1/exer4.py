db0030304501bbe85bbcc6e74a6a3a782387f231
everttonbs <ebs@ic.ufal.br>
2019-01-07 00:40:14 -0300

    Exercicios 4 e 5

---
+++ Lista1/exer4.py
@@ -0,0 +1,13 @@
+
+"""
+Sob certas condições, o número de bactérias B de uma cultura, em função
+do tempo t, medido em horas, é dado por B(t) = 2^t/12. Qual será o número
+de bactérias 6 dias após a hora zero?
+
+"""
+
+t = 6 * 24
+B = 2**(t / 12)
+
+print("B = {}" .format(B))
+print(f"B = {B}")
---
+++ Lista1/exer5.py
@@ -0,0 +1,13 @@
+"""
+O número de bactérias em um meio duplica de hora em hora. Se,
+inicialmente, existem 8 bactérias, qual será o número de bactérias ao fim
+de 10 horas?
+
+"""
+
+numBac = 8
+t = 10
+
+novo_numBac = numBac * 2**t
+print("Numero de bacterias : {}" .format(novo_numBac))
+print(f"Numero de bacterias : {novo_numBac} ")
